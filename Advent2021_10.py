41cbf06c0ec14417c5d54ff335782183b3a48fcf
baczew <71928772+baczew@users.noreply.github.com>
2022-07-18 12:38:27 +0200

    cleanup

--- Advent2021_10.py
+++ Advent2021_10.py
@@ -1,5 +1,49 @@
 from collections import deque
 
+def main():
+
+    data = load_data('Files/input10.txt')
+    part1(data)
+    part2(data)
+
+def part1(data_arg):
+
+    cost_dict = {')': 3, ']': 57, '}': 1197, '>': 25137}
+    bracket_open = ('(', '{', '[', '<')
+    bracket_close = (')', '}', ']', '>')
+
+    cost_list = []
+
+    for line in data_arg:
+        is_corr = is_corrupted(line, bracket_open, bracket_close)
+        if isinstance(is_corr, str):
+            corrupted = is_corr
+            cost_list.append(cost_dict[corrupted]) if cost_dict.get(corrupted, False) else 0
+
+    print(sum(cost_list))
+
+
+def part2(data_arg):
+
+    cost_dict = {')': 1, ']': 2, '}': 3, '>': 4}
+    bracket_open = ('(', '{', '[', '<')
+    bracket_close = (')', '}', ']', '>')
+
+    cost_list = []
+
+    for line in data_arg:
+
+        is_corr = is_corrupted(line, bracket_open, bracket_close)
+
+        if isinstance(is_corr, list):
+            line_score = 0
+            for bracket in is_corr:
+                line_score *= 5
+                line_score += cost_dict.get(bracket)
+            cost_list.append(line_score)
+
+    print(sorted(cost_list)[int(len(cost_list)/2 - 0.5)])
+
 def load_data(file):
 
     with open(file) as op:
@@ -8,36 +52,24 @@ def load_data(file):
 
     return input_list_of_lists
 
-def line_analyze(line, openings, closings):
+def is_corrupted(line, openings, closings):
 
     stack = deque()
     pairs = dict(zip(openings, closings))
 
     for sign in line:
+
         if sign in openings:
             stack.append(sign)
+
         elif pairs[stack.pop()] == sign:
             continue
+
         else:
             return sign
 
+    return [pairs.get(i) for i in reversed(stack)]
 
-def main():
-
-    data = load_data('Files/input10.txt')
-
-    cost_dict = {')': 3, ']': 57, '}': 1197, '>': 25137}
-    bracket_open = ('(', '{', '[', '<')
-    bracket_close = (')', '}', ']', '>')
-
-    cost_list = []
-
-    for line in data:
-
-        corrupted = line_analyze(line, bracket_open, bracket_close)
-        cost_list.append(cost_dict[corrupted]) if cost_dict.get(corrupted, False) else 0
-
-    print(sum(cost_list))
 
 if __name__ == "__main__":
     main()
